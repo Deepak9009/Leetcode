bfb834b93be7855915952ffaec8628819b7c06db
Deepak Rajput <75538314+Deepak9009@users.noreply.github.com>
2023-07-11 17:33:13 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (81.80%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,7 +1,10 @@
 class Solution {
     public int findComplement(int num) {
-       int x = (int) (Math.log(num) / Math.log(2))+1;
-        int m=(1<<x)-1;
-        return m^num;
+        if(num==0) return 1;
+        else{
+             int x = (int) (Math.log(num) / Math.log(2))+1;
+             int m=(1<<x)-1;
+                return m^num;
     }
+}
 }
