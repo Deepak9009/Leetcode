2be735fe30e03c368f074c06d6453eca52fb3b9a
Deepak Rajput <75538314+Deepak9009@users.noreply.github.com>
2023-07-13 11:53:22 +0530

    Time: 3 ms (94.03%), Space: 42.1 MB (79.43%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length())  return false;
         int[] arr1=new int[26];
-        int[] arr2=new int[26];
       int l1=s.length();
         int l2=t.length();
         for(int i=0;i<l1;i++){
@@ -11,10 +10,10 @@ class Solution {
         }
         for(int i=0;i<l2;i++){
             char ch=t.charAt(i);
-            arr2[ch-'a']=arr2[ch-'a']+1;
+            arr1[ch-'a']=arr1[ch-'a']-1;
         }
         for(int i=0;i<26;i++){
-            if(arr1[i]!=arr2[i]){
+            if(arr1[i]!=0){
                 return false;
             }
             
